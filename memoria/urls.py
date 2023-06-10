50942c6f63dabc464bf0b88359995d47fda129d4
Rodrigo <rodrigo@system.cl>
2023-06-10 00:45:01 -0400

    sitio web funcionando con Django

--- memoria/urls.py
+++ memoria/urls.py
@@ -3,7 +3,7 @@ from .views import home, quienes_somos, planes, galeria, contacto
 
 urlpatterns = [
     path("", home, name="home"),
-    path("quienes-somos", quienes_somos, name="quienes_somos"),
+    path("quienes_somos", quienes_somos, name="quienes_somos"),
     path("planes", planes, name="planes"),
     path("galeria", galeria, name="galeria"),
     path("contacto", contacto, name="contacto"),
