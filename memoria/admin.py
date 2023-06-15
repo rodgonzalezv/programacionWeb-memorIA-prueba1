fd12dbd5d47f2c1a6bc15d7626fb08aa796e3f06
Rodrigo <rodrigo@system.cl>
2023-06-15 19:30:31 -0400

    ultimos cambios del branch

--- memoria/admin.py
+++ memoria/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Memorial
+from .models import Memorial, Planes
 # Register your models here.
 admin.site.register(Memorial)
+admin.site.register(Planes)
--- memoria/models.py
+++ memoria/models.py
@@ -15,3 +15,21 @@ class Memorial(models.Model):
 
     def get_absolute_url(self):
         return reverse("Memorial_detail", kwargs={"pk": self.pk})
+
+class Planes(models.Model):
+    id_plan=models.AutoField(primary_key=True)
+    nombre=models.CharField(max_length=50)
+    descripcion=models.TextField()
+    
+
+    
+
+    class Meta:
+        verbose_name = ("Planes")
+        verbose_name_plural = ("Planess")
+
+    def __str__(self):  
+        return self.nombre
+
+    def get_absolute_url(self):
+        return reverse("Planes_detail", kwargs={"pk": self.pk})
