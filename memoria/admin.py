8bfe27a6e76e89cce02e2a642f2f15995e3438ec
nchavezo <nico.chavezo@duocuc.cl>
2023-06-15 22:58:34 -0400

    agrego Familiares a Models

--- memoria/admin.py
+++ memoria/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Memorial, Planes
+from .models import Memorial, Planes, Familiares
 # Register your models here.
 admin.site.register(Memorial)
 admin.site.register(Planes)
+admin.site.register(Familiares)
--- memoria/models.py
+++ memoria/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.core.validators import MaxLengthValidator
 
 class Memorial(models.Model):
     id_memorial=models.AutoField(primary_key=True)
@@ -33,3 +34,41 @@ class Planes(models.Model):
 
     def get_absolute_url(self):
         return reverse("Planes_detail", kwargs={"pk": self.pk})
+
+class Familiares(models.Model):
+    OPC_NACIONALIDAD = (
+        ('AR', 'Argentina'),
+        ('BO', 'Bolivia'),
+        ('BR', 'Brasil'),
+        ('CL', 'Chile'),
+        ('CO', 'Colombia'),
+        ('CR', 'Costa Rica'),
+        ('CU', 'Cuba'),
+        ('EC', 'Ecuador'),
+        ('SV', 'El Salvador'),
+        ('GT', 'Guatemala'),
+        ('HN', 'Honduras'),
+        ('MX', 'México'),
+        ('NI', 'Nicaragua'),
+        ('PA', 'Panamá'),
+        ('PY', 'Paraguay'),
+        ('PE', 'Perú'),
+        ('PR', 'Puerto Rico'),
+        ('DO', 'República Dominicana'),
+        ('UY', 'Uruguay'),
+        ('VE', 'Venezuela'),
+        ('OT','Otra')
+        )
+    
+    id_familiar=models.AutoField(primary_key=True)
+    nombre_familiar=models.CharField(max_length=100)
+    apellidos_familiar=models.CharField(max_length=100)
+    fecha_nacimiento=models.DateField(auto_now=False, auto_now_add=False)
+    fecha_deceso=models.DateField(auto_now=False, auto_now_add=False)
+    nacionalidad = models.CharField(max_length=2, choices=OPC_NACIONALIDAD, default='CL')
+    rut_familiar=models.CharField(max_length=8, validators=[MaxLengthValidator(8)], null=False, default=12345678)
+    dv_familiar=models.CharField(max_length=1, validators=[MaxLengthValidator(1)], null=False, default=0)
+    
+
+    def __str__(self):
+        return self.nombre_familiar, self.apellidos_familiar
