86e29455e6ce1d8a8b3ffd2abde56830997665bd
Rodrigo <rodrigo@system.cl>
2023-06-20 19:35:15 -0400

    login funcionando

--- core/settings.py
+++ core/settings.py
@@ -150,6 +150,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = "login"
 LOGIN_REDIRECT = "home"
-LOGOUT_REDIRECT_URL="login"
+LOGOUT_REDIRECT_URL="home"
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
