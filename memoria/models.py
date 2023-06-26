677d9b8eb5b25f5b9ac162291916f2707a563eae
Rodrigo <rodrigo@system.cl>
2023-06-26 15:56:27 -0400

    ultimo cambio

--- memoria/models.py
+++ memoria/models.py
@@ -1,44 +1,113 @@
 from django.db import models
 from django.conf import settings
+from django.core.validators import MaxLengthValidator
+from django.contrib.auth.hashers import make_password
 
-# Create your models here.
-class Familiar(models.Model):
-    parentezco_choices = (
-        ('MA','Madre'),
-        ('PA','Padre'),
-        ('AB','Abuel@'),
-        ('HE','Herman@'),
-    )
+class Memorial(models.Model):
+    id_memorial=models.AutoField(primary_key=True)
+    nombre=models.CharField(max_length=250)
+       
 
-    nombre = models.CharField(max_length=50)
-    apellidos = models.CharField(max_length=50)
-    fecha_nacimiento = models.DateField()
-    fecha_fallecimiento = models.DateField()
-    parentezco = models.CharField(max_length=2, choices=parentezco_choices)
-    creado_en = models.DateTimeField(auto_now_add=True)
-    actualizado_en = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = ("Memorial")
+        verbose_name_plural = ("Memorials")
 
     def __str__(self):
-        return self.nombre + ' '  + self.apellidos
+        return self.nombre
 
-class Plan(models.Model):
-    nombre = models.CharField(max_length=50)
-    precio = models.IntegerField()
-    cantidad = models.IntegerField(verbose_name='Cantidad Familiares')
+    def get_absolute_url(self):
+        return reverse("Memorial_detail", kwargs={"pk": self.pk})
+
+class Planes(models.Model):
+    id_plan=models.AutoField(primary_key=True)
+    nombre=models.CharField(max_length=50)
+    descripcion=models.TextField()
+    cantidad = models.IntegerField()
+    valor_factor = models.IntegerField()
+
+    class Meta:
+        verbose_name = ("Planes")
+        verbose_name_plural = ("Planess")
+
+    def __str__(self):  
+        return self.nombre
+
+    def get_absolute_url(self):
+        return reverse("Planes_detail", kwargs={"pk": self.pk})
+
+class Familiares(models.Model):
+    OPC_NACIONALIDAD = (
+        ('AR', 'Argentina'),
+        ('BO', 'Bolivia'),
+        ('BR', 'Brasil'),
+        ('CL', 'Chile'),
+        ('CO', 'Colombia'),
+        ('CR', 'Costa Rica'),
+        ('CU', 'Cuba'),
+        ('EC', 'Ecuador'),
+        ('SV', 'El Salvador'),
+        ('GT', 'Guatemala'),
+        ('HN', 'Honduras'),
+        ('MX', 'México'),
+        ('NI', 'Nicaragua'),
+        ('PA', 'Panamá'),
+        ('PY', 'Paraguay'),
+        ('PE', 'Perú'),
+        ('PR', 'Puerto Rico'),
+        ('DO', 'República Dominicana'),
+        ('UY', 'Uruguay'),
+        ('VE', 'Venezuela'),
+        ('OT','Otra')
+        )
+    
+    id_familiar=models.AutoField(primary_key=True)
+    nombre_familiar=models.CharField(max_length=100)
+    apellidos_familiar=models.CharField(max_length=100)
+    fecha_nacimiento=models.DateField(auto_now=False, auto_now_add=False)
+    fecha_deceso=models.DateField(auto_now=False, auto_now_add=False)
+    nacionalidad = models.CharField(max_length=2, choices=OPC_NACIONALIDAD, default='CL')
+    rut_familiar=models.CharField(max_length=8, validators=[MaxLengthValidator(8)], null=False, default=12345678)
+    dv_familiar=models.CharField(max_length=1, validators=[MaxLengthValidator(1)], null=False, default=0)
+    
 
     def __str__(self):
-        return self.nombre + ' $' + str(self.precio)
+        return self.nombre_familiar, self.apellidos_familiar
 
-
-class PlanCliente(models.Model):
-    plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
-    fecha_inicio = models.DateField()
-    fecha_termino = models.DateField()
-
-    creado_en = models.DateTimeField(auto_now_add=True)
-    actualizado_en = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+class Roles(models.Model):
+    id_rol=models.AutoField(primary_key=True)
+    nombre_rol=models.CharField(max_length=100)
 
     def __str__(self):
-        return self.plan.nombre
+        return self.nombre_rol
+
+class Recuerdos(models.Model):
+    id_recuerdo=models.AutoField(primary_key=True)
+    nombre_recuerdo=models.CharField(max_length=100, default='Nombre Recuerdo')
+    descripcion_recuerdo=models.TextField()
+
+    def __str__(self):
+        return self.nombre_recuerdo
+
+class Usuarios(models.Model):
+    id_usuario=models.AutoField(primary_key=True)
+    nombres = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    email=models.EmailField()
+    contraseña = models.CharField(max_length=128)
+    rol = models.ForeignKey(Roles, on_delete=models.CASCADE)
+    
+    def set_password(self, raw_password):
+        self.contraseña = make_password(raw_password)
+
+    def __str__(self):
+        return self.nombres, self.apellidos
+
+class Usuarios_Planes(models.Model):
+    id_plan_usuario=models.AutoField(primary_key=True)
+    id_plan=models.ForeignKey(Planes, on_delete=models.CASCADE)
+    id_usuario=models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+    estado_plan=models.IntegerField()
+
+    def __str__(self):
+        return f"Usuarios_Planes {self.fk}"
+    
