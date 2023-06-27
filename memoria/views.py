438a2b476a6627b5475f534fde7741bc33a4c338
Rodrigo <rodrigo@system.cl>
2023-06-26 20:53:59 -0400

    listo el manejo de suscripciones

--- memoria/views.py
+++ memoria/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login, logout, update_session_auth
 from django.contrib.auth.views import PasswordChangeView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, Group
-from django.shortcuts import render, redirect, get_object_or_404, render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
 from django.core.mail import send_mail
 from django.urls import reverse_lazy
